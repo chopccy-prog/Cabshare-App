c79f5a08617fd2e1caf52a64daf648f470913142
PC <chopccy@gmail.com>
2025-09-05 01:44:56 +0530

    NXT23

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,6 @@ void main() async {
     url: 'https://lrzcnoaqooldywflixkb.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyemNub2Fxb29sZHl3ZmxpeGtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxMTAzMTAsImV4cCI6MjA3MDY4NjMxMH0.82PG2QFVL4lMrqE8E9Es3bJSQZOTpFRo9JXO8TQmxPE',
   );
-  FlutterError.onError = (details) {
-    FlutterError.presentError(details);
-    print(details.toString());
-  };
-  PlatformDispatcher.instance.onError = (error, stack) {
-    print('Platform error: $error\n$stack');
-    return true;
-  };
   runApp(MyApp());
 }
 
@@ -28,7 +20,6 @@ class MyApp extends StatelessWidget {
       routes: {
         '/login': (context) => LoginScreen(),
         '/profile': (context) => ProfileScreen(),
-        // Add other routes (e.g., '/home' for bottom nav)
       },
     );
   }
